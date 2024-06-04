acaff19cedbed795e163032d609c92a4aeb2757f
Betco506 <jvargas@mciconsulting.co>
2024-06-04 13:45:00 -0300

    Add files via upload

---
+++ search.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<title>Search results</title>
+  	 <meta charset="UTF-8"> 
+     <meta name="description" content="Your description">
+     <meta name="keywords" content="Your keywords">
+     <meta name="author" content="Your name">
+      <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+      <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+      <link rel="stylesheet" type="text/css" media="screen" href="css/grid.css">
+      <link rel="stylesheet" type="text/css" media="screen" href="css/superfish.css">
+      <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" /> 
+     <script src="js/jquery-1.7.1.min.js"></script>
+   	 <script src="search/search.js"></script>
+     <script src="js/jquery.hoverIntent.minified.js"></script>
+     <script src="js/jquery.easing.1.3.js"></script>
+     <script src="js/superfish.js"></script>
+<!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]-->
+<!--[if lt IE 9]>
+	<script src="js/html5.js"></script>
+	<link rel="stylesheet" href="css/ie.css"> 
+<![endif]-->
+</head>
+<body>
+
+<div class="main">
+     <div class="main-1">
+	     <div class="div-content">
+	         <div class="head bdb-head">	    
+                 <div class="over-header">
+                       You have not yet registered to our club? <strong><a href="#">Register</a></strong> or <span><a href="#">Log in</a></span>
+                 </div> 
+		         <header>
+                   <div class="logo">
+                         <h1><a href="index.html"></a></h1>
+                   </div>
+                   <a href="#" class="banner-1">
+                       
+                   </a>
+                   <div class="clear"></div>
+                   <div class="nav-search">
+	                   <nav> 
+	                     <ul class="sf-menu">
+	                         <li><a href="index.html">PC</a></li>
+	                          <li><a href="index-1.html">XBOX 360</a></li>
+	                          <li><a href="#">Wii</a>
+	                          <ul>
+	                              <li><a href="#">Lorem ipsum </a></li>
+	                               <li><a href="#">Dolor sit amet </a></li>
+	                               <li><a href="#">Conse </a></li>
+	                               <li><a href="#">Ctetur adipisicing</a></li>
+	                               <li><a href="#">Sed do </a></li>
+	                               <li><a href="#">Eiusmod </a></li>
+	                            </ul>
+	                          </li>
+	                          <li><a href="#">PS2 </a></li>
+	                          <li><a href="#">PS3</a></li> 
+	                          <li><a href="#">PSP</a></li> 
+	                          <li><a href="#">Mobile</a></li> 
+	                      </ul>
+	                      <div class="clear"></div>
+	                    </nav>                        
+                        <div class="div-search">
+	                        <form id="search" action="search.php" method="GET" accept-charset="utf-8">
+	                           <input type="text" name="s" />
+	                           <a onclick="document.getElementById('search').submit()"  class="search_button">Search</a>
+	                        </form>
+			            </div>
+                        <div class="clear"></div>
+                    </div>
+		      </header>
+		   </div> 
+           
+				<!--==============================content================================-->
+		<section id="content">         
+		 <div class="inner-1 bot-content">
+	          <div class="container_12">
+			    <div class="wrapper">
+				    <div class="grid_12">
+                        <article>
+	                     <h2 class="padd-4 bot">Search Results</h2>
+	                     <div id="search-results"></div>
+	                     </article>
+                    </div>
+			         
+			     </div><!--the end of wrapper-->
+			   </div>
+          </div>
+		</section>
+    </div>
+   <aside> 
+     <div class="main-aside">
+         <div class="container_12">
+             <div class="wrapper">
+                 <div class="grid_4">
+                     <h4 class="padd-3 bord-h4">About US</h4>
+                     <span class="col">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </span>
+                     <p class="top-1 bot-1">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                     <a href="#" class="but-aside">read more</a>
+                 </div>
+                 <div class="grid_4">
+                     <h4 class="padd-3 bord-h4">Featured</h4>
+                     <span class="col-1">Dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. </span>
+                     <p class="top-2-1">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                     <p class="top-2-1 bot-2">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+                     <a href="#" class="but-aside">read more</a>
+                 </div>
+                 <div class="grid_4">
+                    <h4 class="padd-3 bord-h4">Top Games:</h4>
+                    <span class="font-12 dis-block top-6">XBOX 360</span>
+                    <p class="col-2 bdb-3 top-0 top-6">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor</p>
+                    <span class="font-12 dis-block top-6 txt-up-n">Mobile</span>
+                    <p class="col-2 bdb-3 top-0 top-6">Dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor</p>
+                    <span class="font-12 dis-block top-6">XBOX 360</span>
+                    <p class="col-2 top-0 top-6 bot-3">Sit amet conse ctetur adipisicing elit</p>
+                    <a href="#" class="but-aside">read more</a>
+                 </div>
+                 
+             </div>
+         </div>
+     </div>
+   </aside>
+		<!--==============================footer=================================-->		
+		<footer>
+	      <div class="main-footer">
+		  	        <p>Games &copy; 2012 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="index-2.html">Privacy Policy</a></p>
+
+	      </div>  
+		</footer>
+    </div>
+</div>
+
+</body>
+</html>
